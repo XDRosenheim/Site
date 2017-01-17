f36166d72f0d27beae5ba91f57c921cbf24a6b4d
Rosenheim <patrickrosenheim@gmail.com>
2017-01-17 15:01:05 +0100

    Minor change.

--- canvas.php
+++ canvas.php
@@ -1,6 +1,33 @@
 <div id="map">
     <div id="hall">
-        <div id="group" class="group-row">
+        <div id="group">
+            <?php
+                for ($i=0; $i < 5; $i++) {
+                    # empty | taken | online | playing | crew | vip
+                    $SEAT_STATUS = "empty";
+                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
+                }
+            ?>
+        </div>
+        <div id="group">
+            <?php
+                for ($i=0; $i < 5; $i++) {
+                    # empty | taken | online | playing | crew | vip
+                    $SEAT_STATUS = "taken";
+                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
+                }
+            ?>
+        </div>
+        <div id="group" class="seat-row">
+            <?php
+                for ($i=0; $i < 5; $i++) {
+                    # empty | taken | online | playing | crew | vip
+                    $SEAT_STATUS = "online";
+                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
+                }
+            ?>
+        </div>
+        <div id="group">
             <?php
                 for ($i=0; $i < 5; $i++) {
                     # empty | taken | online | playing | crew | vip
@@ -9,14 +36,22 @@
                 }
             ?>
         </div>
-        <div id="group" class="group-row">
+        <div id="group">
             <?php
-                for ($i=0; $i < 10; $i++) {
+                for ($i=0; $i < 5; $i++) {
                     # empty | taken | online | playing | crew | vip
-                    $SEAT_STATUS = "playing";
+                    $SEAT_STATUS = "crew";
                     echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
                 }
-
+            ?>
+        </div>
+        <div id="group" class="seat-row">
+            <?php
+                for ($i=0; $i < 5; $i++) {
+                    # empty | taken | online | playing | crew | vip
+                    $SEAT_STATUS = "vip";
+                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
+                }
             ?>
         </div>
     </div>
