40a060f3a7f66c7a9f9252e8b33a732bc3290d98
Rosenheim <patrickrosenheim@gmail.com>
2017-01-17 14:49:24 +0100

    Update
    Now with php.

--- canvas.php
+++ canvas.php
@@ -1,10 +1,23 @@
 <div id="map">
     <div id="hall">
         <div id="group" class="group-row">
-            <div id="seat" class="empty"></div>
-            <div id="seat" class="taken"></div>
-            <div id="seat" class="online"></div>
-            <div id="seat" class="playing"></div>
+            <?php
+                for ($i=0; $i < 5; $i++) {
+                    # empty | taken | online | playing | crew | vip
+                    $SEAT_STATUS = "playing";
+                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
+                }
+            ?>
+        </div>
+        <div id="group" class="group-row">
+            <?php
+                for ($i=0; $i < 10; $i++) {
+                    # empty | taken | online | playing | crew | vip
+                    $SEAT_STATUS = "playing";
+                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
+                }
+
+            ?>
         </div>
     </div>
 </div>
