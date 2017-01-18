15c8f3ebbeaef87907da1bf52657eded496027e6
Rosenheim <patrickrosenheim@gmail.com>
2017-01-18 09:04:55 +0100

    Hey, it's something now.

--- canvas.php
+++ canvas.php
@@ -1,13 +1,29 @@
 <div id="map">
-    <div id="hall">
-        <div id="group" class="seat-hidden">
-            <?php
-                for ($i=0; $i < 23; $i++) {
-                    # empty | taken | online | playing | crew | vip
-                    $SEAT_STATUS = "online";
-                    echo '<div id="seat" class="' . $SEAT_STATUS . '"></div>';
-                }
-            ?>
-        </div>
+  <div id="hall">
+    <div id="group">
+      <?php
+        for ($i=0; $i < 30; $i++) {
+          echo "<div id='row'>";
+          $iteration = 0;
+          for ($j=0; $j < 23; $j++) {
+            # empty | taken | online | playing | crew | vip
+            if ($j == rand(0, 23)) {
+              $SEAT_STATUS = "hidden";
+            } elseif ($j == rand(0, 23) OR $j == rand(0, 23) OR $j == rand(0, 23) OR $j == rand(0, 23)) {
+              $SEAT_STATUS = "playing";
+            } elseif ($j == rand(0, 23) OR $j == rand(0, 23)) {
+              $SEAT_STATUS = "online";
+            } elseif ($j == rand(0, 23) OR $j == rand(0, 23)) {
+              $SEAT_STATUS = "vip";
+            } else {
+              $SEAT_STATUS = "empty";
+            }
+            echo '<div id="seat" class="' . $SEAT_STATUS . '" value="' . $iteration . '"></div>';
+            $iteration += 1;
+          }
+          echo "</div>";
+        }
+      ?>
     </div>
+  </div>
 </div>
--- index.php
+++ index.php
@@ -1,45 +1,46 @@
 <?php
-    if (! isset($HTML_TITLE) OR $HTML_TITLE == "") {
-        $HTML_TITLE = "Front page";
-    }
+  if (! isset($HTML_TITLE) OR $HTML_TITLE == "") {
+    $HTML_TITLE = "Front page";
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head id="head">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $HTML_TITLE?></title>
-    <!-- Self style -->
-    <link id="customStyle" rel="stylesheet" type="text/css" href="css/style.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $HTML_TITLE?></title>
+  <!-- Self style -->
+  <link id="customStyle" rel="stylesheet" type="text/css" href="css/style.css">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
 <body>
-<button onclick="updateStyle()">Style</button>
-<button onclick="updatePage()">Page</button>
-<div class="container" id="content">
-    <div class="row" id="pageCaller">
-    <?php
-        include_once("canvas.php");
-    ?>
-    </div>
+<p style="float: left">Refesh buttons</p>
+<button onclick="updateStyle()" style="float: left">Style</button>
+<button onclick="updatePage()" style="float: left">Page</button>
+<div id="content" style="float: right">
+  <div id="pageCaller">
+  <?php
+    include_once("canvas.php");
+  ?>
+  </div>
 </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script type="text/javascript">
-    var $content = $("#content"); // Where to load.
-    function updatePage() {
-        $content.load("index.php #pageCaller"); // What to load
-    };
-    var $style = $("#head");
-    function updateStyle() {
-        $style.load("index.php #customStyle"); // What to load
-    };
+  var $content = $("#content"); // Where to load.
+  function updatePage() {
+    $content.load("index.php #pageCaller"); // What to load
+  };
+  var $style = $("#head");
+  function updateStyle() {
+    $style.load("index.php #customStyle"); // What to load
+  };
 </script>
 </body>
 </html>
